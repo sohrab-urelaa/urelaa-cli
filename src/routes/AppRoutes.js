13b43d2e2b1c9a53590ad33f03ae6855abf9f9f6
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2024-09-17 00:18:59 +0600

    removed test folders

--- src/routes/AppRoutes.js
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react";
-import Permission from "../helpers/Permission";
-import * as PATH from "./Slugs";
-        //ROUTE_IMPORTS_AREA
-const AppRoutes = [
-    // department
-        //ROUTE_DECLARATION_AREA
-];
-export default AppRoutes;
--- src/routes/Slugs.js
+++
@@ -1,5 +0,0 @@
-// Department
-export const DEPARTMENT_LIST_PATH = `${ROOT_PATH}department-list`;
-export const ADD_DEPARTMENT_PATH = `${ROOT_PATH}add-department`;
-export const EDIT_DEPARTMENT_PATH = `${ROOT_PATH}edit-department`;
-//user//SLUGS_EXPORTS_AREA
